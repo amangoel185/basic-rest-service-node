1ffc2b14dc9fb36673c0341f5773563e4f916284
Aman Goel <aman.goel185@gmail.com>
2020-05-13 04:27:08 +0530

    Implement a POST request

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const sayHola = (req, res) => {
 
 app.get("/", sayHola);
 
+app.post("/add", (req, res) => {
+    const {x, y} = req.body;
+    res.send('The sum of the two numbers is: ${a + b}');
+})
+
 app.listen(7227, () => {
     console.log('The server is running on port 7227.');
 });
